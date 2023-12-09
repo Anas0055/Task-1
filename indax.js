74a638ed1f97914f87bdbe16ed46ff44df01d062
Anas0055 <manaskhan969@gmail.com>
2023-12-09 00:22:40 -0800

    task1

---
+++ indax.js
@@ -0,0 +1,3 @@
+var a = "anas";
+var message = "would you like to learn some Python today?";
+console.log("Hello".concat(a, ",").concat(message));
---
+++ indax.ts
@@ -0,0 +1,3 @@
+let a:string= "anas";
+let message:string="would you like to learn some Python today?";
+console.log(`Hello ${a},${message}` );
